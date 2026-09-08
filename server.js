ec8735770d15167133a5d50eae04fa19ab2b6750
yash more <yash@gmail.com>
2026-09-08 13:38:06 +0530

    Configure backend CORS for deployment

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: process.env.FRONTEND_URL || "*"
+    }
+});
 
 app.use(express.json());
 app.use(express.static("public"));
